269ff85768d8bd4346973c6b4a4b6c165cefe5bf
p-antoine <pierre.antoine.ganaye@gmail.com>
2014-05-13 23:10:32 +0200

    Simplify watcher event decode

--- src/main/scala/com/twitter/finagle/exp/zookeeper/watcher/WatchManager.scala
+++ src/main/scala/com/twitter/finagle/exp/zookeeper/watcher/WatchManager.scala
@@ -2,26 +2,18 @@ package com.twitter.finagle.exp.zookeeper.watcher
 
 import org.jboss.netty.buffer.ChannelBuffer
 import com.twitter.finagle.exp.zookeeper.transport.BufferReader
+import com.twitter.finagle.exp.zookeeper.{WatcherEventBody, WatcherEvent, ReplyHeader}
 
 class WatchManager {
   var watchers: Set[Watcher] = Set[Watcher]()
 }
 
-object WatchManager{
+object WatchManager {
 
-  def decode(buffer: ChannelBuffer) = {
+  def decode(buffer: ChannelBuffer)= {
     val bw = BufferReader(buffer)
 
-    bw.readInt // packet size
-
-    if (bw.readInt == -1) {
-      bw.readLong
-      bw.readInt
-
-      val _type = eventType.getEvent(bw.readInt)
-      val _state = zkState.getState(bw.readInt)
-
-      println("Watch event | type: " + _type + " | state: " + _state + " | path: " + bw.readString)
-    }
+    val replyHeader = ReplyHeader.decode(bw)
+    val watcherEventBody = new WatcherEventBody(bw.readInt, bw.readInt, bw.readString)
   }
 }
