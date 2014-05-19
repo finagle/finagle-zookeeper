9108d00fc50e293a3ac97d898f77039d8e57698c
p-antoine <pierre.antoine.ganaye@gmail.com>
2014-05-19 10:50:23 +0200

    Change endpointer arguments

--- src/main/scala/com/twitter/finagle/exp/zookeeper/ZooKeeper.scala
+++ src/main/scala/com/twitter/finagle/exp/zookeeper/ZooKeeper.scala
@@ -5,12 +5,13 @@ import com.twitter.finagle.exp.zookeeper.transport.ZooKeeperTransporter
 import com.twitter.finagle.util.DefaultTimer
 import com.twitter.finagle.client.{Bridge, DefaultClient}
 import com.twitter.finagle.dispatch.{SerialClientDispatcher, PipeliningDispatcher}
-import com.twitter.finagle.{ServiceFactory,Name}
+import com.twitter.finagle.{ServiceFactory, Name}
 import com.twitter.conversions.time._
 import com.twitter.finagle
 import com.twitter.finagle.exp.zookeeper.client.Client
 
-trait ZKRichClient { self: finagle.Client[Request,BufferedResponse] =>
+trait ZKRichClient {
+  self: finagle.Client[Request, BufferedResponse] =>
   def newRichClient(dest: Name, label: String): Client =
     client.Client(newClient(dest, label))
 
@@ -22,20 +23,17 @@ class ZKClient extends com.twitter.finagle.Client[Request, BufferedResponse]
 with ZKRichClient {
   val defaultClient = new DefaultClient[Request, BufferedResponse](
     name = "zookeeper",
-    endpointer = {
-      val bridge = Bridge[Request, BufferedResponse, Request, BufferedResponse](
-        ZooKeeperTransporter,
-        newDispatcher = new SerialClientDispatcher(_)
-      )
-      (sa, sr) => bridge(sa, sr)
-    }
+    endpointer = Bridge[Request, BufferedResponse, Request, BufferedResponse](
+      ZooKeeperTransporter(_, _),
+      newDispatcher = new SerialClientDispatcher(_)
+    )
   )
 
   override def newClient(dest: Name, label: String): ServiceFactory[Request, BufferedResponse] =
     defaultClient.newClient(dest, label)
 }
 
-object Filter{
+object Filter {
   val retry = new RetryingFilter[Request, Response](
     retryPolicy = RetryPolicy.tries(3),
     timer = DefaultTimer.twitter)
--- src/test/scala/com/twitter/finagle/exp/zookeeper/integration/ClientTest.scala
+++ src/test/scala/com/twitter/finagle/exp/zookeeper/integration/ClientTest.scala
@@ -253,6 +253,7 @@ class ClientTest extends FunSuite with IntegrationConfig {
       }
     })
 
+    Thread.sleep(10000)
     disconnect
   }
 }
