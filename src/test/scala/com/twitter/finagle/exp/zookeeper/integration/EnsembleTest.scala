bda9994e227154320561c7fbeff025c2aad521be
p-antoine <pierre.antoine.ganaye@gmail.com>
2014-07-17 17:41:02 +0200

    commit before transfer

---
+++ src/test/scala/com/twitter/finagle/exp/zookeeper/integration/EnsembleTest.scala
@@ -0,0 +1,20 @@
+package com.twitter.finagle.exp.zookeeper.integration
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class EnsembleTest extends FunSuite with IntegrationConfig {
+  test("change host"){
+
+  }
+
+  test("add hosts"){
+
+  }
+
+  test("remove hosts"){
+
+  }
+}
---
+++ src/test/scala/com/twitter/finagle/exp/zookeeper/integration/StressTest.scala
@@ -0,0 +1,11 @@
+package com.twitter.finagle.exp.zookeeper.integration
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class StressTest extends FunSuite with IntegrationConfig {
+
+
+}
