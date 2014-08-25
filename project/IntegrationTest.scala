cfcecaf28becd64babdbffde6e0a6f9dbe98cae7
Pierre-Antoine Ganaye <pierre.antoine.ganaye@gmail.com>
2014-08-25 22:44:06 +0200

    Fix #52

--- project/IntegrationTest.scala
+++ project/IntegrationTest.scala
@@ -12,7 +12,7 @@ object IntegrationTest {
 
   lazy val fileParser: Initialize[Task[Seq[Config]]] = Def.task {
     val path = System.getProperty("user.dir") +
-      "/integrationTesting/src/main/resources/testConfig.csv"
+      "/integration/src/main/resources/testConfig.csv"
     val source = scala.io.Source.fromFile(path)
     val configs = source.getLines().toSeq map { line =>
       val Array(version, modes, commands, extras) = line.split(";")
@@ -38,17 +38,65 @@ object IntegrationTest {
       case "quorum" =>
         ("sh "
           + System.getProperty("user.dir")
-          + "/integrationTesting/src/main/resources/scripts/runQuorumMode.sh "
+          + "/integration/src/main/resources/scripts/runQuorumMode.sh "
           + s"$version $URL"
           ).!!
 
       case "standalone" =>
         ("sh "
           + System.getProperty("user.dir")
-          + "/integrationTesting/src/main/resources/scripts/runStandaloneMode.sh "
+          + "/integration/src/main/resources/scripts/runStandaloneMode.sh "
           + s"$version $URL"
           ).!!
     }
+  }
+
+  def checkEnv: Initialize[InputTask[Unit]] = Def.inputTask {
+    val args: Seq[String] = spaceDelimited("<arg>").parsed
+    val Seq(mode) = args
+
+    def recursiveCheckEnv(mode: String): Unit = {
+      mode match {
+        case "quorum" =>
+          if (isQuorumAvailable) Unit
+          else {
+            Thread.sleep(2000)
+            recursiveCheckEnv(mode)
+          }
+
+        case "standalone" =>
+          if (isServerAvailable) Unit
+          else {
+            Thread.sleep(2000)
+            recursiveCheckEnv(mode)
+          }
+      }
+    }
+
+    recursiveCheckEnv(mode)
+  }
+
+  def isServerAvailable: Boolean = {
+    val rep = ("echo mntr" #| "nc 127.0.0.1 2181" !!)
+    rep.contains("standalone")
+  }
+
+  def isQuorumAvailable: Boolean = {
+    def evalServer(ip: String, port: String): Int = {
+      val rep = ("echo mntr" #| s"nc $ip $port" !!)
+      if (rep.contains("leader")) 2
+      else if (rep.contains("follower")) 1
+      else 0
+    }
+
+    val total = Seq(
+      ("127.0.0.1", "2181"),
+      ("127.0.0.1", "2182"),
+      ("127.0.0.1", "2183")
+    ).foldLeft(0) { (acc, ad) => acc + evalServer(ad._1, ad._2) }
+
+    if (total == 4) true
+    else false
   }
 
   def cleanEnv(mode: String, version: String): Unit = {
@@ -56,14 +104,14 @@ object IntegrationTest {
       case "quorum" =>
         ("sh "
           + System.getProperty("user.dir")
-          + "/integrationTesting/src/main/resources/scripts/stopAndCleanQuorumMode.sh "
+          + "/integration/src/main/resources/scripts/stopAndCleanQuorumMode.sh "
           + version
           ).!!
 
       case "standalone" =>
         ("sh "
           + System.getProperty("user.dir")
-          + "/integrationTesting/src/main/resources/scripts/stopAndCleanStandaloneMode.sh "
+          + "/integration/src/main/resources/scripts/stopAndCleanStandaloneMode.sh "
           + version
           ).!!
     }
@@ -89,16 +137,11 @@ object IntegrationTest {
     sequence(commandsTasks ++ extrasTasks: _*)
   }
 
-  def sequence(tasks: Initialize[Task[Unit]]*): Initialize[Task[Unit]] = {
-    tasks.reverse.reduceLeft { (a, b) =>
-      val combinedTask: Initialize[Task[Unit]] = {
-        a.zip(b).apply { case (task1, task2) =>
-          task1.dependsOn(task2)
-        }
-      }
-      combinedTask
+  def sequence(tasks: Initialize[Task[Unit]]*): Initialize[Task[Unit]] =
+    tasks match {
+      case Seq() => Def.task { () }
+      case Seq(x, xs@_*) => Def.taskDyn { val _ = x.value; sequence(xs: _*) }
     }
-  }
 
   def integrationTestTask(
     testRunner: InputKey[Unit]
@@ -107,6 +150,7 @@ object IntegrationTest {
       config.modes map { mode =>
         sequence(
           configEnv.toTask(s" ${ config.version } $mode"),
+          checkEnv.toTask(s" $mode"),
           runTests(testRunner, mode, config.commands, config.extras) andFinally {
             cleanEnv(mode, config.version)
           }
