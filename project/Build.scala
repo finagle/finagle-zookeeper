c3beebf9f2eb5c2e3593431d42e2d4a0e9b9e3ce
Bartosz Jankiewicz <bartosz.jankiewicz@gmail.com>
2014-12-26 02:09:03 +0100

    Upgrade to Finagle 6.24 and Scala 2.11

--- project/Build.scala
+++ project/Build.scala
@@ -2,26 +2,30 @@ import sbt._
 import Keys._
 
 object finaglezk extends Build {
-  val FinVersion = "6.17.0"
+
+  val FinVersion = "6.24.0"
+
+  val ScalatestVersion = "2.2.1"
 
   logLevel := Level.Info
 
-  val baseSettings = Defaults.defaultSettings ++ Seq(
+
+  val baseSettings = Defaults.coreDefaultSettings ++ Seq(
     libraryDependencies ++= Seq(
-      "org.scalatest" %% "scalatest" % "1.9.2",
-      "com.twitter" %% "finagle-core" % FinVersion,
-      "junit" % "junit" % "4.11" % "test",
-      "org.mockito" % "mockito-all" % "1.9.5" % "test",
-      "org.slf4j" % "slf4j-api" % "1.7.7"
+      "org.scalatest"     %% "scalatest"      % ScalatestVersion,
+      "com.twitter"       %% "finagle-core"   % FinVersion,
+      "junit"              % "junit"          % "4.11" % "test",
+      "org.mockito"        % "mockito-all"    % "1.9.5" % "test",
+      "org.slf4j"          % "slf4j-api"      % "1.7.7"
     ),
     resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
-      "releases" at "http://oss.sonatype.org/content/repositories/releases",
+      "releases"            at "http://oss.sonatype.org/content/repositories/releases",
       "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-      "twitter-repo" at "http://maven.twttr.com"
+      "twitter-repo"        at "http://maven.twttr.com"
     )
   )
 
-  val publishSettings : Seq[sbt.Project.Setting[_]] = Seq(
+  val publishSettings : Seq[Def.Setting[_]] = Seq(
     publishTo := Some("newzly releases" at "http://maven.newzly.com/repository/internal"),
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     publishMavenStyle := true,
@@ -30,10 +34,10 @@ object finaglezk extends Build {
   )
 
   lazy val buildSettings = Seq(
-    organization := "com.twitter.finagle",
+    organization := "com.twitter",
     version := FinVersion,
-    crossScalaVersions := Seq("2.9.2", "2.10.4"),
-    logLevel := Level.Debug
+    crossScalaVersions := Seq("2.10.4", "2.11.4"),
+    logLevel := Level.Info
   )
 
   lazy val root = Project(
